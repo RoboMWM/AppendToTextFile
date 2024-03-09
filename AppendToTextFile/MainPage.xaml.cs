04f5c3059113f54adab784953d9fcbacd77a7f30
RoboMWM <robomwm@users.noreply.github.com>
2024-03-09 00:27:05 -0800

    working prototype
    If I use a textbox I only get the first line for some reason. Since I
    plan on always copying all text anyways, it doesn't matter and a
    textblock is ok. Can't be bothered to refactor the rest of the names
    tho.

--- AppendToTextFile/MainPage.xaml.cs
+++ AppendToTextFile/MainPage.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.DataTransfer;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +30,58 @@ namespace AppendToTextFile
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void AppendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (appendTextBox.Text.Length == 0)
+                return;
+
+            AppendToFile(appendTextBox.Text);
+        }
+
+        private async void PrintButton_Click(object sender, RoutedEventArgs e)
+        {
+            fileContentsTextBox.Text = await GetAppendFileAsync();
+        }
+
+        private async void AppendToFile(string textToAppend)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(textToAppend);
+            try
+            {
+                StorageFile appendFile = await localFolder.CreateFileAsync("append.txt", CreationCollisionOption.OpenIfExists);
+                await FileIO.AppendTextAsync(appendFile, sb.ToString());
+            }
+            catch (Exception e)
+            {
+                await new MessageDialog("error occurred when appending text: " + e.Message, "uh oh spaghettio").ShowAsync();
+            }
+        }
+
+        private async Task<string> GetAppendFileAsync()
+        {
+            try
+            {
+                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+                StorageFile appendFile = await localFolder.GetFileAsync("append.txt");
+                return await FileIO.ReadTextAsync(appendFile);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            //fileContentsTextBox.SelectAll();
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetText(fileContentsTextBox.Text);
+            Clipboard.SetContent(dataPackage);
         }
     }
 }
